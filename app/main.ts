306995a17b8fa548234c42b12e12a01e149ed015
Mohamad Bastin <mohamadbastin@gmail.com>
2024-04-28 18:15:35 +0200

    add command execution to replica

--- app/main.ts
+++ app/main.ts
@@ -42,7 +42,7 @@ async function main() {
 
   const server: net.Server = net.createServer((connection: net.Socket) => {
     connection.on("data", (data: Buffer) => {
-      console.log("starting command processing for ", instance.role, data.toString());
+      console.log("starting command processing for ", instance.role, data);
       const command: string = data.toString().trim();
       if (instance.isMaster) {
         addReplicaConnection(command, connection);
